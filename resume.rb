17c3f818cbd630afb313a3c17cfedb9df37820e8
Christian Wattengård <cwattengard@gmail.com>
2010-11-22 04:05:10 -0800

    Oppdaterte med eget navn og utf8

--- resume.rb
+++ resume.rb
@@ -8,7 +8,7 @@ require 'rdiscount'
 require 'maruku'
 
 get '/' do
-   title = resume_data.split("\n").first
+   title = resume_data.lines.first.strip
    #oops 1.8.7 only?
     #resume_data.lines.first.strip
    resume = RDiscount.new(resume_data, :smart).to_html
